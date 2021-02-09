767dc861115355e7c489b148e0bc98ee4f75349a
Denis Yakimchuk <denis.yakimchuk@gmail.com>
2021-02-09 02:01:21 +0200

    page object broken! WIP

---
+++ pageObects/actions.ts
@@ -0,0 +1,10 @@
+// import * as playwright from 'playwright';
+// let page = playwright.chromium.launch({ headless: false, slowMo: 50 })
+
+// export default class Actions {
+
+//     async navigate(url: string = 'http://bing.com', timeout: number = 0) {
+//         await page.goto(url)
+//         await page.waitForTimeout(timeout)
+//     }
+// }
--- specs/demo2.ts
+++ specs/demo2.ts
@@ -4,10 +4,23 @@ import * as playwright from 'playwright';
 
 describe("My First Suite", () => {
   it('123', async () => {
+    console.time('start')
     console.log('starting tests')
     let chrome = await playwright.chromium.launch({ headless: false, slowMo: 50 })
     let page = await chrome.newPage()
-    await page.goto('http://google.com')
+
+    async function navigate(url: string = 'http://bing.com', timeout: number = 0) {
+
+      await page.goto(url)
+      await page.waitForTimeout(timeout)
+
+    }
+
+    await navigate('http://google.com', 8000)
+
+    await navigate()
+    //await page.goto('http://google.com')
     await page.waitForTimeout(10 * 1000)
+    console.timeEnd('start')
   });
 });
