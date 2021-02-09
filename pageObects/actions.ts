eb00eac676002a58861481761497c7ff059ceaeb
Sergey Golovin <sk13man@gmail.com>
2021-02-09 05:24:35 +0200

    нафиксил всякого

--- pageObects/actions.ts
+++ pageObects/actions.ts
@@ -1,10 +1,10 @@
-// import * as playwright from 'playwright';
-// let page = playwright.chromium.launch({ headless: false, slowMo: 50 })
+import {chromium} from 'playwright'
 
-// export default class Actions {
-
-//     async navigate(url: string = 'http://bing.com', timeout: number = 0) {
-//         await page.goto(url)
-//         await page.waitForTimeout(timeout)
-//     }
-// }
+export default class Actions {
+    async startTest(url: string, headlessStatus: boolean) {
+        let browser = await chromium.launch({ headless: headlessStatus, args: ['--start-maximized'] })
+        let context = await browser.newContext({ viewport: null });
+        let page = await context.newPage();
+        await page.goto(url, { waitUntil: 'networkidle' });
+    }
+}
